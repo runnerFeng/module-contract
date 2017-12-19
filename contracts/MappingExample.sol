a50edc93e2686c5ae2d613b2c0206fd260c77241
liufeng <631819061@qq.com>
2017-12-19 14:30:39 +0800

    commit

---
+++ contracts/MappingExample.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.0;
+
+
+/**
+ * @author jinx
+ * @date 2017/12/19 10:35
+ * Desc:
+ */
+contract MappingExample {
+    mapping(address => uint) public balances;
+    uint balances
+    ;
+
+}
