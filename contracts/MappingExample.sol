2d8514f5542388a75017edb7671a3ec5e54d12c2
liufeng <631819061@qq.com>
2017-12-19 18:35:27 +0800

    commit

--- contracts/MappingExample.sol
+++ contracts/MappingExample.sol
@@ -8,7 +8,21 @@ pragma solidity ^0.4.0;
  */
 contract MappingExample {
     mapping(address => uint) public balances;
-    uint balances
-    ;
 
+    function update(uint amount)returns (address addr){
+        balances[msg.sender] = amount;
+        return msg.sender;
+    }
+}
+
+contract MappingUser {
+    address  conAddr;
+    address  userAddr;
+
+    function f()returns (uint amount) {
+        conAddr = hex("0xf2bd5de8b57ebfc45dcee97524a7a08fccc80aef");
+        userAddr = hex("0xca35b7d915458ef540ade6068dfe2f44e8fa733c");
+
+        return MappingExample(conAddr).balances(userAddr);
+    }
 }
